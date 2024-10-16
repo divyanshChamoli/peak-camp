84a95952e4b2fad5ca27ddbd410569948da099eb
Divyansh Chamoli <divyanshchamoli567@gmail.com>
2024-10-16 10:49:03 +0530

    backend changes for deployment

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -16,6 +16,13 @@ app.use(cors(
         credentials: true
     }
 ))
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // res.setHeader('Access-Control-Allow-Credentials', 1);
+    next();
+});
 app.use(express.json())
 app.use("/camp",campRouter)
 app.use("/user",userRouter)
