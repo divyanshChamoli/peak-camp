f3e789b33c97849c887facc2cdfc1db4b04cfbab
Divyansh Chamoli <divyanshchamoli567@gmail.com>
2024-10-16 05:53:16 +0530

    backend changes for deployment

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -8,7 +8,12 @@ const userRouter=require("./Routes/user")
 const reviewRouter=require("./Routes/review")
 
 const app: Express=express()
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://peakcamp.vercel.app", "http://localhost:5173/"],
+        credentials: true
+    }
+))
 app.use(express.json())
 app.use("/camp",campRouter)
 app.use("/user",userRouter)
